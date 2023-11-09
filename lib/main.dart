4a947c85e3da02bc41dbe2cf11670b6b4945c279
isaak330 <isaak_lol@icloud.com>
2023-11-09 13:33:41 +0500

    -first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main() async => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
